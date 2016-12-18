cb9f58abb0cc1cc8c10846de4771c1141b91167d
Makoto Tone <makotaiko@yahoo.co.jp>
2016-12-19 04:01:50 +0900

    求められる機能を実現するための方針をコメントに追加

--- routes/getTreeData.js
+++ routes/getTreeData.js
@@ -17,43 +17,43 @@ var credentials = services['cloudantNoSQLDB'][0].credentials;
 var host = credentials.host;
 var port = credentials.port;
 var options = {
-	cache: true,
-	raw: false,
-	secure: true,
-	auth: {
-		username: credentials.username,
-		password: credentials.password
-	}
+    cache: true,
+    raw: false,
+    secure: true,
+    auth: {
+        username: credentials.username,
+        password: credentials.password
+    }
 };
 
 // データベース接続
 var cloudant = Cloudant({
-	account: credentials.username,
-	password: credentials.password
+    account: credentials.username,
+    password: credentials.password
 });
 var db = cloudant.db;
 
 router.post('/Test', function(req, res) {
-	//res.setHeader('Content-Type', 'text/plain');
-	res.send(getTreeJSON(0));
-	
+    //res.setHeader('Content-Type', 'text/plain');
+    getTreeJSON(0, res);
+
 });
 
-var getTreeJSON = function(parent_id) {
-	
-	db.use("remixtree").search('test', 'test_search', {
-		q: 'parent_id:"' + parent_id + '"' 
-	}, function(er, result) {
-		var returnValue = [];
-		if (er) {
-			throw er;
-		}
+var dbSearch = db.use("remixtree").search;
 
-		for(var i=0;i<result.rows.length;i++){
-			returnValue.push({"id":result.rows[i].id,"children":getTreeJSON(result.rows[i].id)});
-		}
-		return returnValue;
-	});
+var getTreeJSON = function(parent_id, res) {
+    var sendingValue = [];
+    var finishedFlag = false;
+    //callback Root
+
+    //親子関係だけでツリーを作成するのは、非同期で検索結果が来る以上無理なので
+    //ツリーグループというカラムを一つ追加し、ツリーのIDを持たせることとする。
+    //これにより、あるツリーに属するデータを全取得、そのデータをもとに親子関係を作る
+    //ということが可能になる
+    var aaa = dbSearch('test', 'test_search', {
+        q: 'parent_id:"' + parent_id + '"'
+    }, function(){});
+    res.send(aaa);
 }
 
 module.exports = router;
